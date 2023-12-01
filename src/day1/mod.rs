cff36752730517fd9801348fe9a85e5bf83382b4
Jared Cohen <jaredjtc@gmail.com>
2023-12-01 01:57:21 -0500

    Cleanup code per paul

--- src/day1/mod.rs
+++ src/day1/mod.rs
@@ -8,50 +8,36 @@ pub enum Answer {
     String(String),
 }
 
-fn get_substring(line: &String, idx: usize, len: i8, reversed: bool) -> String {
-    let substr = &line[idx..cmp::min(idx + len as usize, line.len())];
-    if reversed {
-        return substr.chars().rev().collect();
-    }
-    substr.to_string()
-}
-
-fn is_written_number(line: &String, idx: usize, reversed: bool) -> i64 {
-    let mut value = match get_substring(line, idx, 3, reversed).as_str() {
-        "one" => 1,
-        "two" => 2,
-        "six" => 6,
+fn is_written_number(line: &str, idx: usize) -> i64 {
+    match &line[idx..cmp::min(idx + 3, line.len())] {
+        "one" | "eno" => return 1,
+        "two" | "owt" => return 2,
+        "six" | "xis" => return 6,
         _ => -1,
     };
-    if value != -1 {
-        return value;
-    }
 
-    value = match get_substring(line, idx, 4, reversed).as_str() {
-        "zero" => 0,
-        "four" => 4,
-        "five" => 5,
-        "nine" => 9,
+    match &line[idx..cmp::min(idx + 4, line.len())] {
+        "zero" | "orez" => return 0,
+        "four" | "ruof" => return 4,
+        "five" | "evif" => return 5,
+        "nine" | "enin" => return 9,
         _ => -1,
     };
-    if value != -1 {
-        return value;
-    }
 
-    return match get_substring(line, idx, 5, reversed).as_str() {
-        "three" => 3,
-        "seven" => 7,
-        "eight" => 8,
+    return match &line[idx..cmp::min(idx + 5, line.len())] {
+        "three" | "eerht" => 3,
+        "seven" | "neves" => 7,
+        "eight" | "thgie" => 8,
         _ => -1,
     };
 }
 
-fn get_first_digit(line: &String, reversed: bool) -> i64 {
+fn get_first_digit(line: &str) -> i64 {
     for (idx, character) in line.chars().enumerate() {
         if character.is_digit(10) {
             return character.to_digit(10).unwrap() as i64;
         }
-        let digit = is_written_number(line, idx, reversed);
+        let digit = is_written_number(line, idx);
         if digit != -1 {
             return digit;
         }
@@ -60,8 +46,8 @@ fn get_first_digit(line: &String, reversed: bool) -> i64 {
 }
 
 fn get_value_for_line(line: String) -> i64 {
-    let first_val = get_first_digit(&line, false);
-    let last_val = get_first_digit(&line.chars().rev().collect(), true);
+    let first_val = get_first_digit(&line);
+    let last_val = get_first_digit(&line.chars().rev().collect::<String>());
 
     first_val * 10 + last_val
 }
@@ -76,7 +62,7 @@ pub fn day1() -> Answer {
     Answer::Int(result)
 }
 
-fn read_file(filename: &str) -> io::Lines<io::BufReader<File>> {
+fn read_file(filename: &str) -> impl Iterator<Item = Result<String, io::Error>> {
     let file = File::open(filename).unwrap();
-    io::BufReader::new(file).lines()
+    io::BufReader::new(file).lines().into_iter()
 }
